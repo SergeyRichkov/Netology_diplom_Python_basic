ce0ebed4b5652b609a176b2ca8ccc042d6696472
Sergey Richkov <sergey.rychkov.2016@gmail.com>
2020-07-18 16:51:53 +0700

    make classes

--- foto_from_vk_to_yandex.py
+++ foto_from_vk_to_yandex.py
@@ -18,70 +18,67 @@ import time
 ##with open('response.txt', 'w') as f:
 ##    json.dump(data, f, ensure_ascii=False, indent=2)
 
+def get_photo_information():
+    with open('response.txt', encoding='utf8') as f:
+        response_json = json.load(f)
+    items_list = response_json['response']['items']
 
-with open('response.txt', encoding='utf8') as f:
-    response_json = json.load(f)
+    sizes_tuple = ('w', 'z', 'y', 'x', 'r', 'q', 'p', 'o', 'm', 's')
+    size_list_final = []
+    like_list = []
+    json_file = []
 
-items_list = response_json['response']['items']
+    for photo in items_list:         
+        like_list.append(photo['likes']['count'])    
+        sizes_list_sorted = []
+        number = items_list.index(photo)
+        for i in sizes_tuple:
+            for photo_type_dict in range(len(items_list[number]['sizes'])):
+                if i in items_list[number]['sizes'][photo_type_dict]['type']:
+                    sizes_list_sorted.append(items_list[number]['sizes'][photo_type_dict])
+        size_list_final.append(sizes_list_sorted[0])
+                                  
+    
+    for photo1 in items_list:   
+        how_many_likes = photo1['likes']['count']   
+        photo_type = size_list_final[items_list.index(photo1)]['type'] 
+        load_date = time.gmtime(photo1['date'])
+        if like_list.count(how_many_likes) > 1:
+            json_file.append({'file_name' : f"{how_many_likes}" + '.jpeg' + f"__{time.strftime('%Hh%Mm %d%b %Y', load_date).replace(' ', '_')}", 'size' : photo_type})
+        else:
+            json_file.append({'file_name' : f"{how_many_likes}" + '.jpeg', 'size' : photo_type})
 
-sizes_tuple = ('w', 'z', 'y', 'x', 'r', 'q', 'p', 'o', 'm', 's')
+    data1 = json_file
+    with open('json_file.json', 'w') as f:
+        json.dump(data1, f, ensure_ascii=False, indent=2)
+
+    return  (json_file,size_list_final, like_list)
 
 
-size_list_itog = []
-like_list = []
+            
 
-for photo in items_list:
-    like_list.append(photo['likes']['count'])    
-    sizes_list_sorted = []
-    number = items_list.index(photo)
-    for i in sizes_tuple:
-        for photo_type_dict in range(len(items_list[number]['sizes'])):
-            if i in items_list[number]['sizes'][photo_type_dict]['type']:
-                sizes_list_sorted.append(items_list[number]['sizes'][photo_type_dict])
-    size_list_itog.append(sizes_list_sorted[0])
-  
-              
-                
-
-
-json_file = []
-for photo1 in items_list:   
-    how_many_likes = photo1['likes']['count']   
-    photo_type = size_list_itog[items_list.index(photo1)]['type']
-    load_date = time.gmtime(photo1['date'])
-    if like_list.count(how_many_likes) > 1:
-        json_file.append({'file_name' : f"{how_many_likes}" + '.jpeg' + f"__{time.strftime('%Hh%Mm %d%b %Y', load_date).replace(' ', '_')}", 'size' : photo_type})
-    else:
-        json_file.append({'file_name' : f"{how_many_likes}" + '.jpeg', 'size' : photo_type})
-
-data1 = json_file
-with open('json_file.json', 'w') as f:
-    json.dump(data1, f, ensure_ascii=False, indent=2)
+def upload_to_ya_disk(how_many_photos = 2):
+    param = {'path': 'disk:/from_VK_photo', 'overwrite' : False }
+    header = {'Authorization':'AgAAAABC-feRAADLW4D33gzr_E7Uj9e3uLtiL24',}
+    create_folder = requests.put(
+                'https://cloud-api.yandex.net:443/v1/disk/resources',
+                params=param,
+                headers=header)
         
+    for element in range(how_many_photos):
+        param = {'path': f"disk:/from_VK_photo/{get_photo_information()[0][element]['file_name']}",
+                  'url' : f"{get_photo_information()[1][element]['url']}"}
+       
+        put_photo = requests.post(
+                'https://cloud-api.yandex.net:443/v1/disk/resources/upload',
+                params=param,
+                headers=header)
+        
+        print(put_photo.text)
+    return
 
-print(len(size_list_itog))
-
-param = {'path': 'disk:/from_VK_photo', 'overwrite' : True }
-header = {'Authorization':'AgAAAABC-feRAADLW4D33gzr_E7Uj9e3uLtiL24',}
-create_folder = requests.put(
-            'https://cloud-api.yandex.net:443/v1/disk/resources',
-            params=param,
-            headers=header)
-print(create_folder.text)
-    
-for element in range(len(size_list_itog)):
-    param1 = {'path': f"disk:/from_VK_photo/{json_file[element]['file_name']}",
-              'url' : f"{size_list_itog[element]['url']}"}
-   
-    put_photo = requests.post(
-            'https://cloud-api.yandex.net:443/v1/disk/resources/upload',
-            params=param1,
-            headers=header)
-    
-    print(put_photo.text)
-
-
-    
+upload_to_ya_disk(3)
+        
     
     
 
