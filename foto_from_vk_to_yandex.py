fb388b3b71371021cad7dc8876a3a1135bc6a982
Sergey Richkov <sergey.rychkov.2016@gmail.com>
2020-07-19 21:10:55 +0700

    token fix

--- foto_from_vk_to_yandex.py
+++ foto_from_vk_to_yandex.py
@@ -3,7 +3,7 @@ import json
 import time
 from tqdm import tqdm, trange
 
-##token = '958eb5d439726565e9333aa30e50e0f937ee432e927f0dbd541c541887d919a7c56f95c04217915c32008'
+##YaDisktoken 'AgAAAABC-feRAADLW4D33gzr_E7Uj9e3uLtiL24'
 
 ##with open('response.txt', 'w') as f:
 ##    json.dump(data, f, ensure_ascii=False, indent=2)
@@ -14,12 +14,24 @@ class YaUploader:
         self.vk_id = vk_id
         self.token = token
 
+    def get_id(self):       
+        params = {'user_ids': self.vk_id,
+          'access_token': ('958eb5d439726565e933'
+                  '3aa30e50e0f937ee432e927f0dbd541c541887d919a7c56f95c04217915c32008'),
+          'v':'5.89'
+          }
+        response = requests.get('https://api.vk.com/method/users.get', params = params)
+        response.json()
+        ID = response.json()['response'][0]['id']
+        return ID
+
     def get_photo_information(self):
-        params = {'owner_id': self.vk_id,
+        params = {'owner_id': self.get_id(),
           'album_id': 'profile',
-          'extended' : 1,
+          'extended': 1,
           'photo_sizes': 1,
-          'access_token': self.token,
+          'access_token':('958eb5d439726565e933'
+                  '3aa30e50e0f937ee432e927f0dbd541c541887d919a7c56f95c04217915c32008'),
           'v':'5.89'
           }
         response = requests.get('https://api.vk.com/method/photos.get', params = params)
@@ -40,15 +52,11 @@ class YaUploader:
                 for photo_type_dict in range(len(items_list[number]['sizes'])):
                     if i in items_list[number]['sizes'][photo_type_dict]['type']:
                         sizes_list_sorted.append(items_list[number]['sizes'][photo_type_dict])
-            size_list_final.append(sizes_list_sorted[0])
-
-
-
-                                  
+            size_list_final.append(sizes_list_sorted[0])                                  
     
         for photo1 in items_list:   
             how_many_likes = photo1['likes']['count']   
-            photo_type = size_list_final[items_list.index(photo1)]['type'] 
+            photo_type = size_list_final[items_list.index(photo1)]['type']
             load_date = time.gmtime(photo1['date'])
             if like_list.count(how_many_likes) > 1:
                 json_file.append({'file_name' : f"{how_many_likes}" + '.jpeg' + f"__{time.strftime('%Hh%Mm %d%b %Y', load_date).replace(' ', '_')}", 'size' : photo_type})
@@ -58,18 +66,18 @@ class YaUploader:
         data1 = json_file
         with open('json_file.json', 'w') as f:
             json.dump(data1, f, ensure_ascii=False, indent=2)
-        return  (json_file,size_list_final, like_list)
+        return  (json_file, size_list_final, like_list)
         return            
 
     def upload_to_ya_disk(self, how_many_photos = 2):
-        param = {'path': 'disk:/from_VK_photo', 'overwrite' : False }
-        header = {'Authorization':'AgAAAABC-feRAADLW4D33gzr_E7Uj9e3uLtiL24',}
+        param = {'path': 'disk:/from_VK_photo', 'overwrite': False}
+        header = {'Authorization': self.token}
         create_folder = requests.put(
                     'https://cloud-api.yandex.net:443/v1/disk/resources',
                     params=param,
                     headers=header)
             
-        for element in tqdm(range(how_many_photos), total = 5):
+        for element in tqdm(range(how_many_photos)):
             param = {'path': f"disk:/from_VK_photo/{self.get_photo_information()[0][element]['file_name']}",
             'url' : f"{self.get_photo_information()[1][element]['url']}"}
            
@@ -85,7 +93,7 @@ if __name__ == '__main__':
     token = input('Введите токен пользователя Яндекс.Диск:\n')
     
     uploader = YaUploader(ID, token)
-    result = uploader.upload_to_ya_disk(5)
+    result = uploader.upload_to_ya_disk(3)
 
 
 
